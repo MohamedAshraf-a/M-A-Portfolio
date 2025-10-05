cb754bcdd165f727550172eda3d8c9f4de5753ec
Mohamed Ashraf <pop164412@gmail.com>
2025-10-05 23:44:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Mohamed-Ashraf-Portfolio/", // 👈 اسم الريبو
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
